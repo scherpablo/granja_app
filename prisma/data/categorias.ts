7079299643e202564a3f4dbf2b7e3c6ec5f0a1ae
scherpablo <scherpablo@gmail.com>
2023-10-14 11:11:24 -0300

    upload project

---
+++ prisma/data/categorias.ts
@@ -0,0 +1,20 @@
+const categorias = [
+  {
+    icono: "pollo",
+    nombre: "Pollo",
+  },
+  {
+    icono: "congelados",
+    nombre: "Congelados",
+  },
+  {
+    icono: "quesos",
+    nombre: "Quesos",
+  },
+  {
+    icono: "almacen",
+    nombre: "Almacén",
+  },
+];
+
+export { categorias };
